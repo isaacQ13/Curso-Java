e5e8e5df3f57bf04783fe85c7f7b8a406695b0a9
Isaac Quinto <isaac.quinto@ufpe.br>
2023-06-07 15:04:11 -0300

    Adição da aula sobre a funcionalidade do import

---
+++ src/Fundamentos/Import.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args) {
+        java.lang.String b = "Boa Noite";
+        java.lang.System.out.println(b);
+
+        String a = "Bom dia";
+        System.out.println(a);
+
+        Date d = new Date();
+        System.out.println(d);
+    }
+}
