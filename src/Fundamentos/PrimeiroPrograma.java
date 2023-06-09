f46a751c16ac5ab8fc95564f46496e1589a1322d
Isaac Quinto <isaac.quinto@ufpe.br>
2023-06-09 00:44:38 -0300

    Atualizações

---
+++ src/Fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package Fundamentos;
+/**
+ *
+ * @author Isaac Bastos
+ * @since JDK1.0
+ */
+public class PrimeiroPrograma {
+    /**
+     * ...
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Sentença de código termina com ;
+        // Mais uma linha...
+        // Fim
+        System.out.println("Primeiro programa Parte #01!");
+
+        /*
+         * Linha 1
+         * Linha 2
+         * Linha 3
+         */
+        System.out.println("Primeiro programa Parte #02!");
+    }
+}
