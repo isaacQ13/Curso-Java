bd81917d6153660425bf1ccc205f905c1da06786
Isaac Quinto <isaac.quinto@ufpe.br>
2023-06-13 14:53:50 -0300

    Atualização do package Fundamentos = 1.1

--- src/Fundamentos/Wrappers.java
+++ src/Fundamentos/Wrappers.java
@@ -15,6 +15,20 @@ public class Wrappers {
         System.out.println(b.byteValue());
         System.out.println(s.toString());
         System.out.println(i * 3);
+        System.out.println(l / 3);
+
+        Float f = 123.10F;
+        System.out.println(f);
+
+        Double d = 1234.56778;
+        System.out.println(d);
+
+        Boolean bo = Boolean.parseBoolean("True");
+        System.out.println(bo);
+        System.out.println(bo.toString().toUpperCase());
+
+        Character c = '#'; //char
+        System.out.println(c + "...");
         entrada.close();
     }
 }
