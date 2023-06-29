26ffa93a9b043575b0fa164b20189bc2ee934d62
Isaac Quinto <isaac.quinto@ufpe.br>
2023-06-29 13:32:41 -0300

    Atualização do curso 1.4

---
+++ src/Controle/For1.java
@@ -0,0 +1,18 @@
+package Controle;
+
+public class For1 {
+    public static void main(String[] args) {
+
+        for(int contador = 1; contador <= 10; contador++){
+            System.out.println(contador);
+        }
+        int x = 2;
+        for(;x<=10;){
+            System.out.println(x);
+            x++;
+        }
+        for(;true;){
+            System.out.println("Fim!");
+        }
+    }
+}
--- src/Controle/WhileDeterminado.java
+++ src/Controle/WhileDeterminado.java
@@ -3,5 +3,10 @@ package Controle;
 public class WhileDeterminado {
     public static void main(String[] args) {
 
+        int contador = 1;
+        while (contador <= 10) {
+            System.out.printf("i = %d\n",contador);
+            contador++;
+        }
     }
 }
