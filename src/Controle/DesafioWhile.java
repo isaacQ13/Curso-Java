f19b35c522425260fd62e056b5499fff02269978
isaacQ13 <isaac.quinto@ufpe.br>
2024-02-27 15:25:34 -0300

    Desafio While

--- src/Controle/DesafioWhile.java
+++ src/Controle/DesafioWhile.java
@@ -11,6 +11,18 @@ public class DesafioWhile {
         double nota =0;
         double total = 0;
 
+        while (nota != -1){
+            System.out.println("Informe a nota: ");
+            nota = entrada.nextDouble();
+
+            if (nota >= 0 && nota <= 10){
+                total += nota;
+                quantidadeDeNotas++;
+            }
+        }
+        // Calcular Media
+        double media = total / quantidadeDeNotas;
+        System.out.println("A Sua Media é: " + media);
         entrada.close();
     }
 }
