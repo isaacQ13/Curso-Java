1243576deb6d50bc9efca7228930a5d8389e34d2
isaacQ13 <isaac.quinto@ufpe.br>
2024-02-27 15:14:23 -0300

    Primeira Edição

---
+++ src/Controle/DesafioWhile.java
@@ -0,0 +1,16 @@
+package Controle;
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        int quantidadeDeNotas =0;
+        double nota =0;
+        double total = 0;
+
+        entrada.close();
+    }
+}
